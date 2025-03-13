e4d05846e91625e7ff463a473120f1c16090524d
LiamQuinlan-Carleton <liamquinlan@cmail.carleton.ca>
2025-03-13 16:09:35 -0400

    Fixed run stuff

--- player.gd
+++ player.gd
@@ -9,45 +9,51 @@ extends CharacterBody2D
 @export var jump_buffer : float #Earliest time jump button can be pressed and still allow jump when reaches floor
 
 #Run
-@export var speed : float
+@export var speed : float #Max run speed
 @export var acceleration : float
 @export var floor_friction : float
+@export var air_resistance : float
 
 #Misc
 @export var gravity : float
 
 #Timer Nodes
-@export var jump_timer : Timer
-@export var jump_buffer_timer : Timer
+@export var jump_timer : Timer #Time with constant jump velocity
+
+#Player can press jump before touching ground and still jump once they land. 
+#This is the time allowed between pressing button and jumping
+@export var jump_buffer_timer : Timer 
 #@export var cyote_time_timer : Timer
 
-#Velocity is done this way to allow the player to move faster than their max run speed using other sources
-#var move_velocity : Vector2 #Velocity from player movements (run, jump, etc.)
-#var bonus_velocity : Vector2 #Veclocity from other sources (grapple hook, dash, etc.)
+#Logic variables
+var is_jumping : bool #Is actively holding space to move upwards. Is false when space is let go or jump time runs out
+var want_jump : bool #True when player has pressed space. False when jump starts or after buffer window passes
+var can_jump: bool #True  when player is able to jump
 
-var is_jumping : bool
-var can_jump : bool
+var l_dir : int = 1 #last direction key pressed. -1 = Left, 1 = Right
 
-var direction : int = 1
-
+#The code bellow is in no way organized or easy to read. I apologize in advance.
 func _process(delta: float) -> void:
 	
 	velocity.y += gravity
 	
+	if Input.is_action_pressed("Shift"):
+		velocity.x -= 1000 * delta
+	
 	#Jump control
 	if Input.is_action_just_pressed("Jump"):
-		can_jump = true
+		want_jump = true
 		jump_buffer_timer.start(jump_buffer)
 	
 	if Input.is_action_just_released("Jump"):
-		can_jump = false
+		want_jump = false
 		is_jumping = false
 		if velocity.y < 0:
 			velocity.y += jump_end_force
 	
-	if can_jump and is_on_floor():
+	if want_jump and is_on_floor():
+		want_jump = false
 		is_jumping = true
-		can_jump = false
 		jump_timer.start(jump_length)
 	
 	if is_jumping:
@@ -55,31 +61,44 @@ func _process(delta: float) -> void:
 	
 	#Run control
 	if Input.is_action_just_pressed("Right"):
-		direction = 1
+		l_dir= 1
 	elif Input.is_action_just_released("Right") and Input.is_action_pressed("Left"):
-		direction = -1
+		l_dir = -1
 	
 	if Input.is_action_just_pressed("Left"):
-		direction = -1
+		l_dir = -1
 	elif Input.is_action_just_released("Left") and Input.is_action_pressed("Right"):
-		direction = 1
+		l_dir = 1
 	
 	if Input.is_action_pressed("Right") or Input.is_action_pressed("Left"):
-		velocity.x += acceleration * direction * delta
-		if velocity.x > speed:
-			velocity.x = speed
-		if velocity.x < -speed:
-			velocity.x = -speed
-	elif velocity.x < -floor_friction * delta or velocity.x > floor_friction * delta:
-		if is_on_floor():
-			velocity.x -= direction * floor_friction * delta
+		if sign(velocity.x) * velocity.x <= speed:
+			velocity.x += acceleration * l_dir * delta
+			if velocity.x > speed:
+				velocity.x = speed
+			if velocity.x < -speed:
+				velocity.x = -speed
 	else:
-		velocity.x = 0
+		apply_friction(floor_friction, air_resistance, is_on_floor(), delta)
 	
 	move_and_slide()
+
+
+func apply_friction(f_frict : float, a_resist : float, floor : bool, delta : float):
+	
+	var dir = sign(velocity.x)
+	
+	var vel_to_remove : float
+	if is_on_floor():
+		vel_to_remove = f_frict
+	else:
+		vel_to_remove = a_resist
+	
+	velocity.x -= dir * vel_to_remove * delta
+	if dir * velocity.x < 0:
+		velocity.x = 0
 
 func _on_jump_length_timeout() -> void:
 	is_jumping = false
 
 func _on_jump_buffer_timeout() -> void:
-	can_jump = false
+	want_jump = false
