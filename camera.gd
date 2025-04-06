6b7f71e15d9189180329f83423cfdba39b7102be
LiamQuinlan-Carleton <liamquinlan@cmail.carleton.ca>
2025-04-06 00:07:54 -0400

    two camera types

--- camera.gd
+++ camera.gd
@@ -3,9 +3,13 @@ extends Camera2D
 @export var distance : float
 @export var max_distance : float
 @export var snap : float
+@export var follow_mouse : bool
 
 func _process(delta: float) -> void:
-	position.x = lerp(position.x, distance * get_parent().velocity.x, snap)
-	if sign(position.x) * position.x > max_distance:
-		position.x = sign(position.x) * max_distance
+	if follow_mouse:
+		position = lerp(position, distance * get_local_mouse_position(), snap)
+	else:
+		position.x = lerp(position.x, distance * get_parent().velocity.x, snap)
+		if sign(position.x) * position.x > max_distance:
+			position.x = sign(position.x) * max_distance
 	#zoom = Vector2(1/get_parent().velocity.length(), 1/get_parent().velocity.length())
