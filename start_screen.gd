ef4fd5c22eb334dcc638e7889731befee1cd421e
MarbleCore <deadgame3030@gmail.com>
2025-04-07 05:06:46 -0400

    Title and tweaks

--- start_screen.gd
+++ start_screen.gd
@@ -1,12 +1,25 @@
 extends Control
 
+@onready var title = $Title
+@onready var sfx = $SFX
+@onready var label = $Label
+
+var can_start = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	label.hide()
+	sfx.play()
+	title.play("Startup")
+	await get_tree().create_timer(4.1).timeout
+	can_start = true
+	while true:
+		label.show()
+		await get_tree().create_timer(1.2).timeout
+		label.hide()
+		await get_tree().create_timer(0.8).timeout
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Jump"):
+	if Input.is_action_just_pressed("Jump") and can_start:
 		get_tree().change_scene_to_file("res://level_select.tscn")
